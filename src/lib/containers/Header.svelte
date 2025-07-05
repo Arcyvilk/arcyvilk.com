88e38d6933a5e839ac38a2eb3cb1ec96fd29e141
Arcyvilk <arcyvilk@pm.me>
2025-07-05 21:42:50 +0200

    refactor: 💡 create and style a header

--- src/lib/containers/Header.svelte
+++ src/lib/containers/Header.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
+  import Button from '$lib/components/Button.svelte'
+
+  const onThemeChange = (e: MouseEvent) => {
+    const htmlElement = document.querySelector('html')
+    if (!htmlElement) return
+
+    const currentTheme = htmlElement.getAttribute('data-theme')
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
+    htmlElement.setAttribute('data-theme', newTheme)
+  }
 </script>
 
-<header class="flex flex-row items-center justify-between p-3">
-  <div>:3</div>
-  <h1>Yay, I am a header :3</h1>
-  <div>:3</div>
+<header
+  class="bg-foreground-bg text-foreground-text sticky flex flex-row items-center justify-between gap-1 p-3 shadow-lg"
+>
+  <h1 class="font-c text-xl font-bold uppercase"><a href="/">Arcyvilk.com</a></h1>
+  <div><Button label="Theme" onclick={onThemeChange} /></div>
 </header>
