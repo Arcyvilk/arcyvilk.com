189b269086d854a03e5398c88e6bcb6194cb5ae5
Arcyvilk <arcyvilk@pm.me>
2025-07-05 20:12:13 +0200

    refactor: 💡 renaming and restructuring files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import Header from '$lib/components/Header.svelte'
   import Navigation from '$lib/components/Navigation.svelte'
-  import '../app.css'
 
   let { children } = $props()
 </script>
