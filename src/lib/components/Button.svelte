a87b78f2334c735da9e16ecff6140d610e16bf07
Arcyvilk <arcyvilk@pm.me>
2025-07-05 21:42:19 +0200

    feat: 🎸 add Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { HTMLButtonAttributes } from 'svelte/elements'
+
+  interface ButtonProps extends HTMLButtonAttributes {
+    label: string
+  }
+  let allProps: ButtonProps = $props()
+
+  const { label, ...rest } = allProps
+</script>
+
+<button
+  class="bg-accent-bg text-accent-text cursor-pointer rounded-lg px-3 py-2 font-bold shadow-lg"
+  {...rest}>{label}</button
+>
