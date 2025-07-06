b679242ab479b1a84448d41993a895e2c0082980
Arcyvilk <arcyvilk@pm.me>
2025-07-06 13:35:42 +0200

    feat: 🎸 add Taskbar component

---
+++ src/lib/containers/Taskbar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Button from '$lib/components/Button.svelte'
+  import Divider from '$lib/components/Divider.svelte'
+</script>
+
+<footer class="sticky bottom-0 left-0 w-full">
+  <div
+    class="bg-foreground-bg text-foreground-text border-t-highlight-bg flex gap-1 border-t-4 [border-style:groove] p-1"
+  >
+    <Button label="Start" />
+    <Divider />
+  </div>
+</footer>
