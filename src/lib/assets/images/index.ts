b0e13a303141b9820768b1d928f0c0808a43e59a
Arcyvilk <arcyvilk@pm.me>
2025-07-06 13:44:11 +0200

    feat: 🎸 add RecycleBin and Documents images

--- src/lib/assets/images/index.ts
+++ src/lib/assets/images/index.ts
@@ -1,6 +1,8 @@
+import Documents from '$lib/assets/images/documents.webp'
 import PC from '$lib/assets/images/pc.webp'
+import RecycleBin from '$lib/assets/images/bin.webp'
 
-const images = { PC }
+const images = { Documents, PC, RecycleBin }
 
 export type ImageType = keyof typeof images
 
