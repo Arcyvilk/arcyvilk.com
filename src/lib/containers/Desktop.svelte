e3cadcba1aa86578a5ccd7730494a8f613f59cfa
Arcyvilk <arcyvilk@pm.me>
2025-07-06 13:35:32 +0200

    feat: 🎸 add Desktop compoennt

---
+++ src/lib/containers/Desktop.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  let { children } = $props()
+</script>
+
+<div class="bg-background-bg text-background-text flex h-screen flex-col overflow-hidden">
+  {@render children()}
+</div>
