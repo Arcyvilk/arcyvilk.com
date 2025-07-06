200c4d7bbdd106e1faea70ee48eac4ff16ab8201
Arcyvilk <arcyvilk@pm.me>
2025-07-06 13:59:21 +0200

    feat: 🎸 add Script image

--- src/lib/assets/images/index.ts
+++ src/lib/assets/images/index.ts
@@ -1,8 +1,9 @@
 import Documents from '$lib/assets/images/documents.webp'
 import PC from '$lib/assets/images/pc.webp'
 import RecycleBin from '$lib/assets/images/bin.webp'
+import Script from '$lib/assets/images/script.webp'
 
-const images = { Documents, PC, RecycleBin }
+const images = { Documents, PC, RecycleBin, Script }
 
 export type ImageType = keyof typeof images
 
