b54e317b0b9f69ff674bc2e6bc1b5496490794d7
Arcyvilk <arcyvilk@pm.me>
2025-07-06 13:35:06 +0200

    feat: 🎸 add DesktopIcon component

---
+++ src/lib/components/DesktopIcon.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { ImageType } from '$lib/assets/images'
+  import Image from '$lib/components/Image.svelte'
+
+  type DesktopIconProps = {
+    icon: ImageType
+    label: string
+    alt: string
+  }
+  const { alt, icon, label }: DesktopIconProps = $props()
+</script>
+
+<button
+  data-testid="desktop-icon"
+  class="button-outline flex h-34 max-h-34 w-32 max-w-32 flex-col items-center justify-center gap-2 overflow-hidden p-1"
+>
+  <Image image={icon} {alt} className="h-16 w-16 aspect-square" />
+  <p class="wrap text-foreground-text line-clamp-2 [font-family:Win95FA] text-xl leading-5">
+    {label}
+  </p>
+</button>
+
+<style>
+  .button-outline {
+    &:active,
+    &:focus,
+    &:focus-visible {
+      outline-color: var(--color-foreground-bg);
+      outline-width: 2px;
+      outline-style: dotted;
+    }
+  }
+</style>
