09e7f67983f795819d3e6a19bd0520a6b98f3120
Arcyvilk <arcyvilk@pm.me>
2025-07-06 13:34:58 +0200

    feat: 🎸 add Image component

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import images, { type ImageType } from '$lib/assets/images'
+
+  type ImageProps = {
+    image: ImageType
+    alt?: string
+    className?: string
+  }
+
+  const { image, alt, className }: ImageProps = $props()
+  const src = images[image]
+</script>
+
+<img {src} {alt} class={className} />
