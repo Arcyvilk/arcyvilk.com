c9d0cf097da295de3f186ef030952715bf033b36
Arcyvilk <arcyvilk@pm.me>
2025-07-06 13:36:02 +0200

    refactor: 💡 renderlist of DesktopIcons in the Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
-  import Header from '$lib/containers/Header.svelte'
-  import Navigation from '$lib/containers/Navigation.svelte'
+  import type { ImageType } from '$lib/assets/images'
+  import DesktopIcon from '$lib/components/DesktopIcon.svelte'
+  import Desktop from '$lib/containers/Desktop.svelte'
+  import Taskbar from '$lib/containers/Taskbar.svelte'
 
-  let { children } = $props()
+  type DesktopIcon = {
+    icon: ImageType
+    label: string
+    alt: string
+  }
+  const pc = {
+    icon: 'PC',
+    label: 'My PC haha hehe lolmao heheszki',
+    alt: 'My PC'
+  }
+  const desktopIcons: DesktopIcon[] = new Array(24).fill(pc)
 </script>
 
-<Header />
-<Navigation />
-{@render children()}
+<Desktop>
+  <div class="flex flex-grow flex-col flex-wrap content-start gap-4 overflow-hidden p-4">
+    {#each desktopIcons as desktopIcon}
+      <DesktopIcon icon={desktopIcon.icon} label={desktopIcon.label} alt={desktopIcon.alt} />
+    {/each}
+  </div>
+
+  <Taskbar />
+</Desktop>
