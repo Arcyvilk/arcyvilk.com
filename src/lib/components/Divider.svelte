483610876b04adf6d64703f82460cbbb840cfa9a
Arcyvilk <arcyvilk@pm.me>
2025-07-06 13:35:19 +0200

    feat: 🎸 add Divider component

---
+++ src/lib/components/Divider.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-row items-center justify-center gap-1">
+  <div
+    class="border-l-shadow-bg border-r-highlight-bg h-full w-0.5 border-2 [border-style:inset]"
+  ></div>
+  <div
+    class="border-l-highlight-bg border-r-shadow-bg h-8/12 w-1.5 border-2 [border-style:outset]"
+  ></div>
+</div>
