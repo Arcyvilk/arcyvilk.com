a2c42cf6b126262476037c781005ee92e785b96f
Arcyvilk <arcyvilk@pm.me>
2025-07-06 13:44:41 +0200

    refactor: 💡 make DesktopIcon outline more Win98

--- src/lib/components/DesktopIcon.svelte
+++ src/lib/components/DesktopIcon.svelte
@@ -10,15 +10,17 @@
   const { alt, icon, label }: DesktopIconProps = $props()
 </script>
 
-<button
-  data-testid="desktop-icon"
-  class="button-outline flex h-34 max-h-34 w-32 max-w-32 flex-col items-center justify-center gap-2 overflow-hidden p-1"
->
-  <Image image={icon} {alt} className="h-16 w-16 aspect-square" />
-  <p class="wrap text-foreground-text line-clamp-2 [font-family:Win95FA] text-xl leading-5">
-    {label}
-  </p>
-</button>
+<div class="flex h-34 max-h-34 w-32 max-w-32 items-center justify-center">
+  <button
+    data-testid="desktop-icon"
+    class="button-outline flex flex-col items-center justify-center gap-2 overflow-hidden p-1"
+  >
+    <Image image={icon} {alt} className="h-16 w-16 aspect-square" />
+    <p class="wrap text-foreground-text line-clamp-2 [font-family:Win95FA] text-xl leading-5">
+      {label}
+    </p>
+  </button>
+</div>
 
 <style>
   .button-outline {
