4c07603413234b1282a19b8a37d018efe2384cb6
ARCY <arcyvilk@pm.me>
2025-08-31 16:48:17 +0000

    refactor: move desktopIcons to its own file

---
+++ src/lib/data/desktopIcons.ts
@@ -0,0 +1,51 @@
+import type { ImageType } from '$lib/assets/images'
+
+export type DesktopIconId = 'pc' | 'documents' | 'bin' | 'script' | 'missingno'
+
+export type DesktopIconProps = {
+  id: DesktopIconId
+  icon: ImageType
+  label: string
+  alt: string
+  hidden?: boolean
+}
+
+export const defaultDesktopIcon: DesktopIconProps = {
+  id: 'missingno',
+  icon: 'MissingNo',
+  label: 'MISSINGNO',
+  alt: 'MISSINGNO',
+  hidden: true
+}
+
+export const desktopIcons: DesktopIconProps[] = [
+  defaultDesktopIcon,
+  {
+    id: 'pc',
+    icon: 'PC',
+    label: 'My Computer',
+    alt: 'My PC',
+    hidden: false
+  },
+  {
+    id: 'documents',
+    icon: 'Documents',
+    label: 'My Documents',
+    alt: 'My Documents',
+    hidden: false
+  },
+  {
+    id: 'bin',
+    icon: 'RecycleBin',
+    label: 'Recycle Bin',
+    alt: 'Recycle Bin',
+    hidden: false
+  },
+  {
+    id: 'script',
+    icon: 'Script',
+    label: 'Completely harmless file, there is nothing to worry about',
+    alt: 'Completely harmless file',
+    hidden: false
+  }
+]
