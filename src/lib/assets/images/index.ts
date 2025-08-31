3df6eb47ceb5efc26713031f7bb1941051da4ef0
ARCY <arcyvilk@pm.me>
2025-08-31 16:48:49 +0000

    refactor: define a MISSINGNO icons for when desktop icon is not found

--- src/lib/assets/images/index.ts
+++ src/lib/assets/images/index.ts
@@ -1,9 +1,10 @@
 import Documents from '$lib/assets/images/documents.webp'
+import MissingNo from '$lib/assets/images/missingno.webp'
 import PC from '$lib/assets/images/pc.webp'
 import RecycleBin from '$lib/assets/images/bin.webp'
 import Script from '$lib/assets/images/script.webp'
 
-const images = { Documents, PC, RecycleBin, Script }
+const images = { Documents, MissingNo, PC, RecycleBin, Script }
 
 export type ImageType = keyof typeof images
 
