4ad89c624e6ca70a023714763ce4b7f31a405dc9
ARCY <arcyvilk@pm.me>
2025-09-04 19:15:39 +0000

    refactor: fade desktop in

--- src/lib/containers/Desktop.svelte
+++ src/lib/containers/Desktop.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+  import { fade } from 'svelte/transition'
+
   let { children } = $props()
 </script>
 
-<div class="bg-desktop-bg text-background-text flex h-screen flex-col overflow-hidden">
+<div
+  transition:fade={{ duration: 300 }}
+  class="bg-desktop-bg text-background-text flex h-screen flex-col overflow-hidden"
+>
   {@render children()}
 </div>
