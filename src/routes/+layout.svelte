ac3ffa5b8665aab2f365bbeb1349464619e0709a
ARCY <arcyvilk@pm.me>
2025-09-06 19:05:50 +0000

    refactor: add CRT overlay effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,5 +10,31 @@
 </script>
 
 <div class="h-screen w-screen bg-white">
+  <div class="crt absolute h-screen w-screen"></div>
   {@render children()}
 </div>
+
+<style>
+  .crt {
+    pointer-events: none;
+    background: linear-gradient(to top, #000000, #000000, #333333, #333333);
+    background-size: cover;
+    background-size: 100% 1px;
+    opacity: 15%;
+    z-index: 99999999999999;
+  }
+
+  @media screen {
+    .crt {
+      animation: scanlines infinite 55s linear;
+    }
+  }
+  @keyframes scanlines {
+    from {
+      background-position: 0 0;
+    }
+    to {
+      background-position: 0 -10px;
+    }
+  }
+</style>
