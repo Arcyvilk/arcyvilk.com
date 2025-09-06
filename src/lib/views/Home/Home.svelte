ec76de3402f4bfd0556930ca15dd9ea3f8ac1173
ARCY <arcyvilk@pm.me>
2025-09-06 19:05:41 +0000

    feat: add Windows98 screen for the start

--- src/lib/views/Home/Home.svelte
+++ src/lib/views/Home/Home.svelte
@@ -6,6 +6,19 @@
   }
 </script>
 
-<div>
-  <button onclick={handleRedirectToDesktop}>Go to Desktop</button>
+<div class="home-bg border-box h-screen w-screen p-[64px]">
+  <div class="desktop-bg border-box flex h-full w-full bg-cover bg-center">
+    <button onclick={handleRedirectToDesktop}>Go to Desktop</button>
+  </div>
 </div>
+
+<style>
+  .home-bg {
+    border: 128px ridge #d5c9a4;
+    background-color: #000000;
+  }
+
+  .desktop-bg {
+    background-image: url('/illustrations/win98.jpg');
+  }
+</style>
