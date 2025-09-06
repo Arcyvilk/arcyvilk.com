d734a562f28ac415f031d2d23a70b7e9aeb43788
ARCY <arcyvilk@pm.me>
2025-09-06 19:06:52 +0000

    refactor: make Go To Desktop button more visible

--- src/lib/views/Home/Home.svelte
+++ src/lib/views/Home/Home.svelte
@@ -8,7 +8,10 @@
 
 <div class="home-bg border-box h-screen w-screen p-[64px]">
   <div class="desktop-bg border-box flex h-full w-full bg-cover bg-center">
-    <button onclick={handleRedirectToDesktop}>Go to Desktop</button>
+    <button
+      class="absolute right-[64px] bottom-[64px] bg-gray-800 p-4 text-white"
+      onclick={handleRedirectToDesktop}>Go to Desktop</button
+    >
   </div>
 </div>
 
