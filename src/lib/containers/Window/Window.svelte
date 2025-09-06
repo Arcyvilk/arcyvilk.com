dc7b8abee1fbfa3c3763f27ecacd40473017100d
ARCY <arcyvilk@pm.me>
2025-09-06 18:10:27 +0000

    refactor: move Window to its own directory

--- src/lib/containers/Window/Window.svelte
+++ src/lib/containers/Window/Window.svelte
@@ -100,15 +100,6 @@
     </header>
 
     <div class="box-border flex-1 overflow-y-auto">
-      <!-- <iframe
-        src={iframe}
-        title="Embedded content"
-        class="h-[80vh] w-[80vw]"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe> -->
-
       <DynamicContent></DynamicContent>
     </div>
   </div>
