b9246332a3c109817af6dcb6effddffcbb6a3f4f
ARCY <arcyvilk@pm.me>
2025-09-07 11:20:25 +0000

    fix: make Window draggable only by header

--- src/lib/containers/Window/Window.svelte
+++ src/lib/containers/Window/Window.svelte
@@ -70,14 +70,16 @@
 <dialog
   class="fixed"
   bind:this={dialog}
-  use:draggable={{ bounds: 'parent' }}
+  use:draggable={{ bounds: 'parent', handle: '.window-drag-handle' }}
   onclose={onWindowClose}
   onclick={onWindowClick}
 >
   <div
     class="window-border bg-window-bg box-border flex size-fit h-auto max-h-[90vh] max-w-[90vw] flex-col"
   >
-    <header class="bg-window-header-bg flex cursor-move items-center justify-between gap-8 p-1">
+    <header
+      class="window-drag-handle bg-window-header-bg flex cursor-move items-center justify-between gap-8 p-1"
+    >
       <div class="flex items-center gap-2">
         {#if icon}
           <Image image={icon} alt="" className="h-6 w-6 aspect-square" />
