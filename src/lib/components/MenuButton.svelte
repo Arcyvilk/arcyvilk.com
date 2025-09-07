3b3d57750220ea47d58eaf122a6f2f449eaf224d
ARCY <arcyvilk@pm.me>
2025-09-07 11:02:20 +0000

    feat: add a new MenuButton component

---
+++ src/lib/components/MenuButton.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  let { label, shortcutLetter } = $props()
+
+  const highlighted = $derived.by(() => {
+    if (!shortcutLetter) return label
+
+    const regex = new RegExp(shortcutLetter, 'gi')
+    return label.replace(regex, (match: string) => `<span class="underline">${match}</span>`)
+  })
+</script>
+
+<button class="menu-button">{@html highlighted}</button>
+
+<style>
+  .menu-button {
+    padding-inline: 12px;
+    margin: 1px;
+    border: 1px solid transparent;
+    box-sizing: border-box;
+
+    &:hover {
+      background-color: #b0b0b0;
+      cursor: pointer;
+      border: 1px solid var(--color-highlight-bg);
+
+      border-bottom-color: var(--color-shadow-bg);
+      border-right-color: var(--color-shadow-bg);
+    }
+
+    &:active {
+      background-color: #b0b0b0;
+      cursor: pointer;
+      border: 1px solid var(--color-highlight-bg);
+
+      border-top-color: var(--color-shadow-bg);
+      border-left-color: var(--color-shadow-bg);
+    }
+  }
+</style>
