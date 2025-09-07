fff073f633da1e609bc0abdfeca14beb22602f72
ARCY <arcyvilk@pm.me>
2025-09-07 12:03:43 +0000

    refactor: add a couple of new Win98 icons

--- src/lib/assets/win98Icons/index.ts
+++ src/lib/assets/win98Icons/index.ts
@@ -1,11 +1,26 @@
-import Documents from '$lib/assets/desktopIcons/documents.webp'
-import Explorer from '$lib/assets/desktopIcons/explorer.webp'
-import Tree from '$lib/assets/desktopIcons/tree.webp'
-import PC from '$lib/assets/desktopIcons/pc.webp'
-import RecycleBin from '$lib/assets/desktopIcons/bin.webp'
-import Script from '$lib/assets/desktopIcons/script.webp'
+import Documents from '$lib/assets/win98Icons/documents.webp'
+import Explorer from '$lib/assets/win98Icons/explorer.webp'
+import Folder from '$lib/assets/win98Icons/folder.webp'
+import PaintFile from '$lib/assets/win98Icons/paintFile.webp'
+import PC from '$lib/assets/win98Icons/pc.webp'
+import RecycleBin from '$lib/assets/win98Icons/bin.webp'
+import Script from '$lib/assets/win98Icons/script.webp'
+import Shortcut from '$lib/assets/win98Icons/shortcut.webp'
+import TextFile from '$lib/assets/win98Icons/textFile.webp'
+import Tree from '$lib/assets/win98Icons/tree.webp'
 
-const win98DesktopIcons = { Documents, Explorer, PC, RecycleBin, Script, Tree }
+const win98DesktopIcons = {
+  Documents,
+  Explorer,
+  Folder,
+  PaintFile,
+  PC,
+  RecycleBin,
+  Script,
+  Shortcut,
+  TextFile,
+  Tree,
+}
 
 export type Win98DesktopIcon = keyof typeof win98DesktopIcons
 
