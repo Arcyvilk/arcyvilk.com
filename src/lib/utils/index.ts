ea2a80708abb5f0decdd3a9f6ebe6ecf3fb0d1be
ARCY <arcyvilk@pm.me>
2025-09-13 14:25:15 +0000

    refactor: remove nonexistant exports

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,2 @@
 export { canvasProtection } from "./canvasProtection"
-export { fetchTextFile, getBase64FromString, getBitmapFromBlob, getBlobFromBase64 } from "./dataUtilities"
 export { preparePreview } from "./preparePreview"
