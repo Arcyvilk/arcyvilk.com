2e67b85e87c46adc7b3e97b1ebe5b8b7f1c7939f
ARCY <arcyvilk@pm.me>
2025-09-13 14:24:03 +0000

    chore: mock env file (no sensitive data included)

---
+++ src/lib/env.ts
@@ -0,0 +1,7 @@
+// Those vars aren't sensitive, so they can live here.
+// Would be cool to parse some paths on the server though!
+
+const CDN_URL = "https://cdn.arcyvilk.com"
+const PREVIEW_PATH = "arcy/files"
+
+export { CDN_URL, PREVIEW_PATH }
