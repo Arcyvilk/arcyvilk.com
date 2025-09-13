6d2c7417b6df9f82b3fddf2c031334615d2b4d12
ARCY <arcyvilk@pm.me>
2025-09-13 12:39:02 +0000

    refactor: display image preview in canvas

--- src/lib/containers/ImagePreview/ImagePreview.svelte
+++ src/lib/containers/ImagePreview/ImagePreview.svelte
@@ -1,11 +1,42 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+
   let { args } = $props<{ args?: { imgSrc: string } }>()
 
   let imgSrc = $derived(args?.imgSrc)
+  let width = $state(400)
+  let height = $state(600)
+
+  onMount(() => {
+    const loadRealImageIntoCanvas = () => {
+      const canvas = document.getElementById('preview-canvas') as HTMLCanvasElement
+      const context = canvas?.getContext('2d')
+
+      if (!context) {
+        console.error('Failed to get canvas context')
+        return
+      }
+
+      const image = new Image()
+      image.src = imgSrc
+
+      image.onload = () => {
+        width = image.width
+        height = image.height
+
+        canvas.width = width
+        canvas.height = height
+
+        context.drawImage(image, 0, 0)
+      }
+    }
+
+    loadRealImageIntoCanvas()
+  })
 </script>
 
 <div
   class="box-border flex min-h-[80vh] min-w-[80vw] flex-1 flex-col items-center justify-start overflow-scroll bg-black"
 >
-  <img class="max-w-full" src={imgSrc} alt="Preview" />
+  <canvas id="preview-canvas" class="max-w-full"></canvas>
 </div>
