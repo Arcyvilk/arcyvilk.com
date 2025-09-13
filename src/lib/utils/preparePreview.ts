7a508e236ae8674c59bb710ade65bf0d699d7eac
ARCY <arcyvilk@pm.me>
2025-09-13 14:24:27 +0000

    refactor: remove unnecessary data utilities file

--- src/lib/utils/preparePreview.ts
+++ src/lib/utils/preparePreview.ts
@@ -1,10 +1,18 @@
-import { fetchTextFile, getBase64FromString, getBitmapFromBlob, getBlobFromBase64 } from "$lib/utils/dataUtilities";
+import { CDN_URL, PREVIEW_PATH } from "$lib/env"
 
-export const preparePreview = async (fileSrc: string) => {
-  const text = await fetchTextFile(fileSrc)
-  const base64 = await getBase64FromString(text)
-  const blob = await getBlobFromBase64(`data:text/plain;base64,${base64}`)
-  const bitmap = await getBitmapFromBlob(blob)
+export const preparePreview = async (id: string) => {
+  const cdnUrl = `${CDN_URL}/${PREVIEW_PATH}/${id}.txt`
+  const response = await fetch(cdnUrl)
+  
+  if (!response.ok) {
+    throw new Error("Not OK :C")
+  }
+
+  const arrayBuffer = await response.arrayBuffer()
+
+  const blob = new Blob([arrayBuffer], { type: "image/png" })
+
+  const bitmap = await createImageBitmap(blob)
 
   return bitmap
 }
